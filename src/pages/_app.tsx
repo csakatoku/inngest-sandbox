69470f7fdead92224fee654a6f303b9dc3f7d498
Chihiro SAKATOKU <csakatoku@gmail.com>
2023-08-12 00:28:26 +0900

    Remove unused frontend codes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
-import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
